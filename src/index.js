7228c75019cf367220ee31f48dfc898df12d0036
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-06-11 18:59:19 -0700

    format data to conform to google calendar api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 require('dotenv').config()
 const get = require('axios').get
 
-const getCalendarData = async () => {
-	const { data } = await get(`${process.env.URL}`)
-	console.log(data)
+const getMatches = async () => {
+	const { data: { events } } = await get(`${process.env.URL}`)
+	const matches = events.map(event => {
+		const nameSplit = event.name.split(' at ')
+		const match = [nameSplit[1],nameSplit[0]].join(' x ')
+		const leagueName = event.league.shortName.replace('Brazil','Brasil')
+		return {
+			'summary': `${match}`,
+			'start': { 'dateTime': `${event.date}` },
+			'end': { 'dateTime': `${event.date}` },
+			'description': `${leagueName}`,
+			'status': 'confirmed',
+			'colorId': '11'
+		}
+	})
+	console.log(matches)
 }
 
-getCalendarData()
+getMatches()
