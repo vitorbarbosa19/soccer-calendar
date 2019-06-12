f37d92f1a1cb3691177c366889c8fd9aee915ee3
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-06-12 16:03:08 -0700

    call api to insert event

--- src/index.js
+++ src/index.js
@@ -3,23 +3,28 @@ const get = require('axios').get
 const calendarAPI = require('node-google-calendar')
 const settings = require('./settings')
 
-const getMatches = async () => {
-	const { data: { events } } = await get(`${process.env.URL}`)
-	const matches = events.map(event => {
-		const nameSplit = event.name.split(' at ')
-		const match = [nameSplit[1],nameSplit[0]].join(' x ')
-		const leagueName = event.league.shortName.replace('Brazil','Brasil')
-		return {
-			'summary': `${match}`,
-			'start': { 'dateTime': `${event.date}` },
-			'end': { 'dateTime': `${event.date}` },
-			'description': `${leagueName}`,
-			'status': 'confirmed',
-			'colorId': '11'
-		}
-	})
-	console.log(matches)
-	const calendar = new calendarAPI(settings)
+try {
+	const getMatches = async () => {
+		const { data: { events } } = await get(`${process.env.URL}`)
+		const matches = events.map(event => {
+			const nameSplit = event.name.split(' at ')
+			const match = [nameSplit[1],nameSplit[0]].join(' x ')
+			const leagueName = event.league.shortName.replace('Brazil','Brasil')
+			return {
+				'summary': `${match}`,
+				'start': { 'dateTime': `${event.date}` },
+				'end': { 'dateTime': `${event.date}` },
+				'description': `${leagueName}`,
+				'status': 'confirmed',
+				'colorId': '11'
+			}
+		})
+		console.log(matches)
+		const calendar = new calendarAPI(settings)
+		const response = await calendar.Events.insert(process.env.CALENDAR_ID, matches[0])
+		console.log(response)
+	}
+	getMatches()
+} catch (error) {
+	console.log(error)
 }
-
-getMatches()
