00fe6b19622ffcdf39c70b4898cf4797fd96cc7c
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-06-14 19:37:09 -0700

    refactor function to format dateTime

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const get = require('axios').get
 const calendarAPI = require('node-google-calendar')
 const settings = require('./settings')
+const formatDateTime = require('./formatDateTime')
 
 try {
 	const getMatches = async () => {
@@ -9,17 +10,17 @@ try {
 		const matches = events.map(event => {
 			const nameSplit = event.name.split(' at ')
 			const match = [nameSplit[1],nameSplit[0]].join(' x ')
+			const matchDate = formatDateTime(event.date)
 			const leagueName = event.league.shortName.replace('Brazil','Brasil')
 			return {
 				'summary': `${match}`,
-				'start': { 'dateTime': `${event.date}` },
-				'end': { 'dateTime': `${event.date}` },
+				'start': { 'dateTime': `${matchDate}` },
+				'end': { 'dateTime': `${matchDate}` },
 				'description': `${leagueName}`,
 				'status': 'confirmed',
 				'colorId': '11'
 			}
 		})
-		console.log(matches)
 		const calendar = new calendarAPI(settings)
 		const response = await calendar.Events.insert(process.env.CALENDAR_ID, matches[0])
 		console.log(response)
